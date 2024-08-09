14e355a19c4cbbc7900a8dafa08cec686a406fe1
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-08-09 16:54:37 +0500

    Create ruRU.lua

---
+++ locale/ruRU.lua
@@ -0,0 +1,30 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("GreatVaultList", "ruRU", true)
+
+if not L then
+	return
+end
+
+L["addonName"] = "Great Vault List"
+
+L["class"] = "Класс"
+L["character"] = "Персонаж"
+L["raid"] = "Рейды"
+L["activities"] = "Активности"
+L["pvp"] = "PvP"
+L["ilevel"] = "ilvl"
+L["mythic"] = "М+"
+L["keystone"] = "Ключ"
+
+L["OpenVault"] = "Открыть Хранилище"
+L["Options"] = "Параметры"
+
+L["opt_windowname"] = "Параметры Great Vault List"
+
+L["opt_option"] = "Параметры"
+
+L["opt_lines_name"] = "Линии"
+L["opt_lines_desc"] = "Количество строк для отображения"
+
+L["opt_column"] = "Столбец"
+L["opt_enabled"] = "Включено"
+L["opt_position"] = "Позиция"
