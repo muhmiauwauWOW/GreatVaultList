6fd45cdebc41f6d7f03ce537c612f62736f7737e
muhmiauwau <fabio@madless.de>
2024-09-18 13:10:21 +0200

    refactor TableHeaderString

---
+++ frames/TableHeaderString.lua
@@ -0,0 +1,64 @@
+local GreatVaultList = LibStub("AceAddon-3.0"):GetAddon("GreatVaultList")
+local L, _ = GreatVaultList:GetLibs()
+
+
+
+GreatVaultListTableHeaderStringMixin = CreateFromMixins(TableBuilderElementMixin);
+
+
+
+function GreatVaultListTableHeaderStringMixin:OnClick()
+	if not self.interactiveHeader then return end
+	self.owner:SetSortOrder(self.sortOrder);
+end
+
+function GreatVaultListTableHeaderStringMixin:OnEnter()
+	if not self.tooltip then return end
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+	GameTooltip:AddLine(self.tooltip);
+	GameTooltip:Show();
+end
+function GreatVaultListTableHeaderStringMixin:OnLeave()
+	if not self.tooltip then return end
+	GameTooltip:Hide();
+end
+
+
+function GreatVaultListTableHeaderStringMixin:Init(owner, headerText, sortOrder, tooltip)
+	self:SetText(headerText);
+
+	local find = _.find(owner.sortHeaders, function(entry) return entry == sortOrder; end)
+	self.interactiveHeader = owner.RegisterHeader and find;
+	self.tooltip = tooltip
+	self.owner = owner;
+	self.sortOrder = sortOrder;
+
+	
+
+	if self.interactiveHeader then
+		owner:RegisterHeader(self);
+		self:UpdateArrow();
+	else
+		self:ClearHighlightTexture()
+		self:ClearPushedTexture()
+		self:SetPushedTextOffset(0, 0)
+		self.Arrow:Hide();
+	end
+end
+
+function GreatVaultListTableHeaderStringMixin:UpdateArrow(reverse)
+	if self.owner.sort == self.sortOrder then 
+		self:SetArrowState(reverse)
+		self.Arrow:Show();
+	else 
+		self.Arrow:Hide();
+	end
+end
+
+function GreatVaultListTableHeaderStringMixin:SetArrowState(reverse)
+	if reverse then
+		self.Arrow:SetTexCoord(0, 1, 1, 0);
+	else 
+		self.Arrow:SetTexCoord(0, 1, 0, 1);
+	end
+end
