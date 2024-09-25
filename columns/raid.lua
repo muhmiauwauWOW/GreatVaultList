baaccf33488bd9f490169702fd18a18011ec5dd0
muhmiauwau <fabio@madless.de>
2024-09-25 17:40:19 +0200

    implement raid sorting functions

--- columns/raid.lua
+++ columns/raid.lua
@@ -14,6 +14,7 @@ local DIFFICULTY_NAMES = {
 	[DifficultyUtil.ID.PrimaryRaidLFR] = "LFR",
 }
 
+local PRIMARY_RAIDS = { DifficultyUtil.ID.PrimaryRaidLFR, DifficultyUtil.ID.PrimaryRaidNormal, DifficultyUtil.ID.PrimaryRaidHeroic, DifficultyUtil.ID.PrimaryRaidMythic };
 
 Column.key = ColumKey
 Column.config = {
@@ -21,25 +22,33 @@ Column.config = {
     ["template"] = "GreatVaultListTableCellTripleTextTemplate",
     ["width"] = 100,
     ["padding"] = 0, 
-    ["header"] =  { key = ColumKey, text = RAIDS, width = 40, canSort = false},
+    ["header"] =  { key = ColumKey, text = RAIDS, width = 40, canSort = true},
     ["subCols"] = 3,
     ["sort"] = {
         ["key"] = ColumKey,
         ["store"] = ColumKey,
     },
     ["sortFn"] = function(a, b, comp)
-        if a[1].level == b[1].level and a[2].level == b[2].level and a[3].level == b[3].level  then
+        local a1Level = _.indexOf(PRIMARY_RAIDS, a[1].level)
+        local b1Level = _.indexOf(PRIMARY_RAIDS, b[1].level)
+        local a2Level = _.indexOf(PRIMARY_RAIDS, a[2].level)
+        local b2Level = _.indexOf(PRIMARY_RAIDS, b[2].level)
+        local a3Level = _.indexOf(PRIMARY_RAIDS, a[3].level)
+        local b3Level = _.indexOf(PRIMARY_RAIDS, b[3].level)
+
+
+        if a1Level == b1Level and a2Level == b2Level and a3Level == b3Level  then
             return comp(a[3].progress, b[3].progress)
         end
        
-        if a[3].level == b[3].level then
-            if a[2].level == b[2].level then
-                return comp(a[1].level, b[1].level)
+        if a1Level == b1Level then
+            if a2Level == b2Level then
+                return comp(a3Level, b3Level)
             end
-            return comp(a[2].level, b[2].level)
+            return comp(a2Level, b2Level)
         end
         
-        return comp(a[3].level, b[3].level)
+        return comp(a1Level, b1Level)
     end,
     ['emptyStr'] = {
         "0/2",
@@ -47,19 +56,19 @@ Column.config = {
         "0/6"
     },
     ["demo"] = function(idx)
-        local keys =  _.keys(DIFFICULTY_NAMES)
-        -- local level = keys[math.random(_.size(keys))]
         local obj = {}
-
-        local progress = math.random(7) - 1
+        local progress = math.random(9) -1
         local threshold = {2, 4, 6}
 
+        local start = math.random(#PRIMARY_RAIDS)
         for i = 1, 3, 1 do
-            local level = keys[math.random(_.size(keys))]
+            start = start - i > 1  and start -  math.random(0,1) or 1
+            local level = PRIMARY_RAIDS[start]
+
             table.insert(obj, {
                 progress = progress,
                 threshold = threshold[i],
-                level = level
+                level = progress >= threshold[i] and level or 0
             })
         end
         return obj
@@ -84,7 +93,7 @@ Column.config = {
     ["populate"] = function(self, data, idx)
         if type(data) ~= "table" then return nil end
         local activity = _.get(data, {idx}, {} )
-        local text = nil -- set default
+        local text = nil -- set default 
         
         if not activity.progress then return nil end
         if not activity.threshold then return nil end
