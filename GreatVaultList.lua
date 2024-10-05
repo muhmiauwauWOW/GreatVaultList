f357a2ee3f79049bcd24170a71b6e73087081577
muhmiauwau <fabio@madless.de>
2024-10-05 18:01:50 +0200

    add ELVUI skin support

--- GreatVaultList.lua
+++ GreatVaultList.lua
@@ -98,6 +98,8 @@ function GreatVaultList:OnInitialize()
 	else
 		GreatVaultListFrame:SetScale(self.db.global.Options.scale)
 	end
+
+	self:ElvSkin()
 end
 
 function GreatVaultList:hideWindow()
@@ -299,4 +301,82 @@ function GreatVaultList:GetVaultState()
 	end
 
 	return "incomplete";
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- support for all the ELVUI fanbois out there
+function GreatVaultList:ElvSkin()
+	if not C_AddOns.IsAddOnLoaded("ElvUI") then return end
+	
+	local E, L, V, P, G = unpack(ElvUI)
+	local S = E:GetModule("Skins")
+
+	GreatVaultListFrame:StripTextures()
+	GreatVaultListFrame:SetTemplate("Transparent")
+
+	S:HandleCloseButton(GreatVaultListFrameCloseButton)
+	
+
+	for _, tab in next, {GreatVaultListFrame.TabSystem:GetChildren() } do
+		S:HandleTab(tab)
+	end
+
+	GreatVaultListFrame.TabSystem:ClearAllPoints()
+	GreatVaultListFrame.TabSystem:Point('TOPLEFT', PlayerSpellsFrame, 'BOTTOMLEFT', -3, 0)
+
+
+	S:HandleEditBox(GreatVaultListFrame.ListFrame.Search)
+	S:HandleButton(GreatVaultListFrame.ListFrame.Filter)
+
+
+	local function HandleHeaders(frame)
+		local maxHeaders = frame.HeaderContainer:GetNumChildren()
+		for i, header in next, { frame.HeaderContainer:GetChildren() } do
+			if not header.IsSkinned then
+				header:DisableDrawLayer('BACKGROUND')
+
+				if not header.backdrop then
+					header:CreateBackdrop('Transparent')
+				end
+
+				header.IsSkinned = true
+			end
+
+			if header.backdrop then
+				header.backdrop:Point('BOTTOMRIGHT', i < maxHeaders and -5 or 0, -2)
+			end
+		end
+	end
+
+	
+	local function HandleSellList(frame)
+		frame:StripTextures()
+		frame:SetTemplate('Transparent')
+
+		S:HandleTrimScrollBar(frame.ScrollBar)
+		frame.ScrollBar:ClearAllPoints()
+		frame.ScrollBar:Point('TOPRIGHT', frame, -10, -16)
+		frame.ScrollBar:Point('BOTTOMRIGHT', frame, -10, 16)
+
+		hooksecurefunc(frame, 'RefreshScrollFrame', HandleHeaders)
+	end
+	HandleSellList(GreatVaultListFrame.ListFrame.ItemList)
+	HandleSellList(GreatVaultListFrame.RaidLootList.ItemList)
+	HandleSellList(GreatVaultListFrame.DungeonLootList.ItemList)
+	HandleSellList(GreatVaultListFrame.DelvesLootList.ItemList)
 end
