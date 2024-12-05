2a51b1c4cedd7343d510a0de5db977ee547db58d
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-12-05 16:57:23 +0500

    Update ruRU.lua

--- locale/ruRU.lua
+++ locale/ruRU.lua
@@ -11,10 +11,14 @@ L["highestReward"] = ""
 L["highestReward_tooltip_title"] = "Наивысшая награда"
 L["highestReward_tooltip_desc"] = "Уровень предмета наивысшей разблокированной награды Великого Хранилища" 
 
+L["class"] = "Класс"
 L["character"] = "Персонаж"
 L["realm"] = "Игровой мир"
 L["ilevel"] = "ilvl"
 L["keystone"] = "Ключ"
+L["highestReward"] = "Наивысшая награда"
+
+L["vaultStatus"] = "Невостребованные награды"
 
 L["OpenVault"] = "Открыть Хранилище"
 
