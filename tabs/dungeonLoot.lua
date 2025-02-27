bc45e312e9225054c88963efd7cde1a5a78e0bcb
muhmiauwau <fabio@madless.de>
2025-02-27 16:39:58 +0100

    Update dungeonLoot.lua

--- tabs/dungeonLoot.lua
+++ tabs/dungeonLoot.lua
@@ -47,84 +47,86 @@ function GreatVaultListDungeonLootListMixin:BuildData()
 	self:AddColumn(L["tabLoot_upgradelvl"], false, L["tabLoot_greatVault"])
 
 
+
 	self.ItemList.data = {
 		{ 
 			PLAYER_DIFFICULTY2,
-			580,
+			619,
 			L["gearTrack_Adventurer"] .. " 4/8",
-			593,
+			632,
 			L["gearTrack_Veteran"] .. " 4/8"
 		},
 		{ 
 			PLAYER_DIFFICULTY6,
-			597,
+			632,
 			L["gearTrack_Veteran"] .. " 4/8",
-			603,
+			642,
 			L["gearTrack_Champion"] .. "  3/8"
 		},
-		{ 
+		{
 			2,
-			597,
-			L["gearTrack_Champion"] .. "  1/8",
-			606,
-			L["gearTrack_Champion"] .. "  4/8"
+			639,
+			L["gearTrack_Champion"] .. " 2/8",
+			649,
+			L["gearTrack_Hero"] .. " 1/6"
 		},
-		{ 
+		{
 			3,
-			597,
-			L["gearTrack_Champion"] .. "  1/8",
-			610,
-			L["gearTrack_Hero"] .. "  1/6"
+			639,
+			L["gearTrack_Champion"] .. " 2/8",
+			649,
+			L["gearTrack_Hero"] .. " 1/6"
 		},
-		{ 
+		{
 			4,
-			600,
-			L["gearTrack_Champion"] .. "  2/8",
-			610,
-			L["gearTrack_Hero"] .. "  1/6"
+			642,
+			L["gearTrack_Champion"] .. " 3/8",
+			652,
+			L["gearTrack_Hero"] .. " 2/6"
 		},
-		{ 
+		{
 			5,
-			603,
-			L["gearTrack_Champion"] .. "  3/8",
-			613,
-			L["gearTrack_Hero"] .. "  2/6"
+			645,
+			L["gearTrack_Champion"] .. " 4/8",
+			652,
+			L["gearTrack_Hero"] .. " 2/6"
 		},
-		{ 
+		{
 			6,
-			606,
-			L["gearTrack_Champion"] .. "  4/8",
-			613,
-			L["gearTrack_Hero"] .. "  2/6"
+			649,
+			L["gearTrack_Hero"] .. " 1/6",
+			655,
+			L["gearTrack_Hero"] .. " 3/6"
 		},
-		{ 
+		{
 			7,
-			610,
-			L["gearTrack_Hero"] .. "  1/6",
-			616,
-			L["gearTrack_Hero"] .. "  3/6"
+			649,
+			L["gearTrack_Hero"] .. " 1/6",
+			658,
+			L["gearTrack_Hero"] .. " 4/6"
 		},
-		{ 
+		{
 			8,
-			610,
-			L["gearTrack_Hero"] .. "  1/6",
-			619,
-			L["gearTrack_Hero"] .. "  4/6"
+			652,
+			L["gearTrack_Hero"] .. " 2/6",
+			658,
+			L["gearTrack_Hero"] .. " 4/6"
 		},
-		{ 
+		{
 			9,
-			613,
-			L["gearTrack_Hero"] .. "  2/6",
-			619,
-			L["gearTrack_Hero"] .. "  4/6"
+			652,
+			L["gearTrack_Hero"] .. " 2/6",
+			658,
+			L["gearTrack_Hero"] .. " 4/6"
 		},
-		{ 
+		{
 			10,
-			613,
-			L["gearTrack_Hero"] .. "  2/6",
-			623,
-			L["gearTrack_Myth"] .. "  1/6"
-		}
+			655,
+			L["gearTrack_Hero"] .. " 3/6",
+			662,
+			L["gearTrack_Myth"] .. " 1/6"
+		},
 	}
+
 
 end
